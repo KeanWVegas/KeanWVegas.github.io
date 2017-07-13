540acc24cf936629257f1d96081b3f948248f7e1
KeanWVegas <keanwvegas@gmail.com>
2017-07-13 09:18:47 -0600

    json updates

---
+++ final_project/javascript/pulls.js
@@ -0,0 +1,9 @@
+//A jQuery function to implement modularization
+
+$(function () {
+    $("#getstarted1").load("/final_project/json/getstarted1.html");
+    $("#getstarted2").load("/final_project/json/getstarted2.html");
+    $("#myhistory1").load("/final_project/json/myhistory1.html");
+    $("#myhistory2").load("/final_project/json/myhistory2.html");
+    $("#myhistory3").load("/final_project/json/myhistory3.html");
+});
