56bef19d24d9e90e4aa2d08d299cf0da02672053
KeanWVegas <keanwvegas@gmail.com>
2017-07-14 10:20:18 -0600

    json update

--- final_project/javascript/ctmodules.js
+++
@@ -1,9 +0,0 @@
-//A jQuery function to implement modularization
-
-$(function () {
-    $("#getstarted1").load("/json/getstarted1.html");
-    $("#getstarted2").load("/json/getstarted2.html");
-    $("#myhistory1").load("/json/myhistory1.html");
-    $("#myhistory2").load("/json/myhistory2.html");
-    $("#myhistory3").load("/json/myhistory3.html");
-});
